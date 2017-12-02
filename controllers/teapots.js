e43bd5a4e28fec514fc49859b0018caa419bcbac
liziscurious <32523128+liziscurious@users.noreply.github.com>
2017-12-02 11:05:31 -0800

    Create and show routes working. Index route now sending items.

--- controllers/teapots.js
+++ controllers/teapots.js
@@ -6,9 +6,43 @@ const router      = express.Router();
 const Teapot      = require('../models/teapots.js');
 // const Comment     = require('../models/comments.js');
 
-// index route
+// index route (GET)
 router.get('/', async (req, res) => {
-  res.send("Teapot Index")
+  try{
+    const allTeapots = await Teapot.find();
+    res.send({allTeapots});
+    // res.send("Welcome to the Teapot Wonder Dome Index")
+  } catch (err) {
+    res.send(err.message);
+  }
 });
+
+// new  (GET)
+
+// create (POST)
+router.post('/', async (req, res) => {
+  try {
+    const newTeapot = await Teapot.create(req.body);
+    res.send({newTeapot});
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
+// show (GET)
+router.get('/:id', async (req, res) => {
+  try {
+    const oneTeapot = await Teapot.findById(req.params.id);
+    res.send({oneTeapot});
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
+// edit (GET)
+
+// update (PUT)
+
+// delete (DELETE)
 
 module.exports  = router;
