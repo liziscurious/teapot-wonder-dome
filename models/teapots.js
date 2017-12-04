d0bcabfc6dba15c9449f366c6f48953202ab682f
liziscurious <32523128+liziscurious@users.noreply.github.com>
2017-12-03 17:12:43 -0800

    Expanded teapots model created stylesheet

--- models/teapots.js
+++ models/teapots.js
@@ -3,7 +3,10 @@ const mongoose      = require('mongoose');
 
 const teapotSchema  = mongoose.Schema({
   url:  { type: String, require: true },
-  submitted_by: { type: String, require: true}
+  nameOfTeapot: { type: String, require: true },
+  material: String,
+  writeUp: { type: String, require: true },
+  submitted_by: { type: String, require: true},
 });
 
 module.exports  = mongoose.model('Teapot', teapotSchema);
